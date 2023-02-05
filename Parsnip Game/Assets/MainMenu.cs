1ad7ef2f5e3269fdf9be9ea004325a6df04ff341
Sappels <58121086+Sappels@users.noreply.github.com>
2023-02-05 13:42:59 +0100

    VolumeData + Further MainMenu Functionality

--- Parsnip Game/Assets/MainMenu.cs
+++ Parsnip Game/Assets/MainMenu.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject optionsMenu;
+
     public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void ActivateOptionsMenu()
+    {
+        optionsMenu.SetActive(!optionsMenu.activeInHierarchy);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
